18b30f08d6b846988148a4fa26e164f7426e0810
yasechkaa <yasya.malimon@yandex.ru>
2026-03-03 21:31:03 +0300

    я крутаяяя

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Запускаем Gunicorn в фоне, привязываем к unix-сокету с правами 0666
-gunicorn --bind unix:/tmp/app.sock -m 0666 --workers 2 --threads 2 wsgi:app --daemon
+gunicorn --bind unix:/tmp/app.sock -m 0666 --workers 2 wsgi:app &
 
 # Запускаем Nginx на переднем плане
 nginx -g "daemon off;"
